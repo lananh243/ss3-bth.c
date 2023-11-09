0ba0f196e47191054917e3729e668c3e78421fee
lananh243 <147390706+lananh243@users.noreply.github.com>
2023-11-09 11:46:58 +0700

    Add files via upload

---
+++ bt2 ss3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	printf("             %s\n","Danh sach sinh vien");
+	printf("| %s | %s      | %s | %s | %s\n","Stt","Ho va ten","Tuoi","So dien thoai","email");
+	printf("| %d   |  %s  | %d   | %s    | %s\n",1,"Nguyen Van A",20,"0904488481","anv@rikkeiacademy.com" );
+	printf("| %d   | %s   | %d   | %s    | %s\n",2,"Nguyen Van B",21,"0904488482","bnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",3,"Nguyen Van C",18,"0904488483","cnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",4,"Nguyen Van D",19,"0904488484","dnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",5,"Nguyen Van E",22,"0904488485","env@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",6,"Nguyen Van F",21,"0904488486","fnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",7,"Nguyen Van G",23,"0904488487","gnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",8,"Nguyen Van H",19,"0904488488","hnnv@rikkeiacademy.com");
+	printf("| %d   | %s   | %d   | %s    | %s\n",9,"Nguyen Van I",18,"0904488489","inv@rikkeiacademy.com");
+	printf("| %d  | %s   | %d   | %s    | %s\n",10,"Nguyen Van K",21,"0904488480","knv@rikkkeiacademy.com");
+} 
